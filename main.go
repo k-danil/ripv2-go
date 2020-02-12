8f65c8466bf1f674c4b2bbf08ef0afe7af11b910
Данил Корымов <danil@imac.ihome.ru>
2020-02-12 18:00:46 +0300

    Multi interface support and comments

--- main.go
+++ main.go
@@ -9,22 +9,31 @@ import (
 
 func main() {
 
-	p := socket()
-	defer p.Close()
+	interfaces := []string{"br0"}
+	pass := "123"
 
+	//Listen for multicast on interfaces
+	p := socket(interfaces)
+	defer socketClose(p, interfaces)
+
+	//Receive packet in buffer
 	for {
-		b := make([]byte, 510) //Maximum size os RIP pdu - 504byte
+		b := make([]byte, 504) //Maximum size of RIP pdu - 504byte
 		s, cm, _, err := p.ReadFrom(b)
 		if err != nil {
 			log.Fatal(err)
 		} else if s != 0 {
+			//Process received packet
 			go func() {
+				//Fill service fields and payload to struct
 				r, err := read(b[:s], cm.IfIndex, cm.Src)
 				if err != nil {
 					log.Fatal(err)
 				}
+				//Parse payload
 				r.parser()
-				err = r.validator("")
+				//Validate over RFC guidline and authenticate with pass
+				err = r.validator(pass)
 				if err != nil {
 					log.Println(err)
 				}
@@ -34,28 +43,43 @@ func main() {
 	}
 }
 
-func socket() *ipv4.PacketConn {
+func socket(ifcn []string) *ipv4.PacketConn {
+	mrip := net.UDPAddr{IP: net.IPv4(224, 0, 0, 9)}
+
 	c, err := net.ListenPacket("udp4", "0.0.0.0:520")
 	if err != nil {
 		log.Fatal(err)
 	}
-	// defer c.Close()
+
 	p := ipv4.NewPacketConn(c)
 
-	br0, err := net.InterfaceByName("br0")
-	if err != nil {
-		log.Fatal(err)
+	for _, ifc := range ifcn {
+		ifi, err := net.InterfaceByName(ifc)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if err := p.JoinGroup(ifi, &mrip); err != nil {
+			log.Fatal(err)
+		}
 	}
 
-	mrip := net.UDPAddr{IP: net.IPv4(224, 0, 0, 9)}
-
-	if err := p.JoinGroup(br0, &mrip); err != nil {
-		log.Fatal(err)
-	}
 	// defer p.LeaveGroup(br0, &mrip)
 	if err := p.SetControlMessage(ipv4.FlagDst, true); err != nil {
 		log.Fatal(err)
 	}
 
 	return p
+}
+
+func socketClose(p *ipv4.PacketConn, ifcn []string) {
+	mrip := net.UDPAddr{IP: net.IPv4(224, 0, 0, 9)}
+
+	for _, ifc := range ifcn {
+		ifi, err := net.InterfaceByName(ifc)
+		if err != nil {
+			log.Fatal(err)
+		}
+		p.LeaveGroup(ifi, &mrip)
+	}
+	p.Close()
 }
